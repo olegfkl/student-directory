8ede4016ac94d5e4869cfeaa18c4fcc0e2b26566
Oleg Grushetskyy <olegfkl@gmail.com>
2017-10-09 22:52:21 +0100

    Modify program to only print names with specific letter

--- directory.rb
+++ directory.rb
@@ -16,14 +16,41 @@ def print_header
   puts "--------------"
 end
 
-def print_students(students)
-  students.each.with_index(1) { |student , index | puts "#{index} #{student[:name]} (#{student[:cohort]} cohort)" }
+def print_students(students, letter=nil)
+  if !letter.nil?
+    sort_by_letter(students, letter)
+  else
+    students.each.with_index(1) do | student , index |
+    puts "#{index} #{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
 end
+
+def sort_by_letter(students, letter)
+  specific_letter = []
+  students.each do | name |
+    if name[:name].downcase.start_with?(letter)
+       specific_letter << { name: name[:name] , cohort: :november }
+    end
+  end
+  puts "You entered #{specific_letter.count} student(s) starting with your criteria letter \"#{letter}\""
+  specific_letter.each.with_index(1) do | name , index |
+      puts "#{index} #{name[:name]} (#{name[:cohort]} cohort)"
+    end
+  end
+
+
+
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 students = input_students
 #Calling methods
 print_header
-print_students(students)
+print_students(students, 'b')
 print_footer(students)
+
+
+#Modify your program to only print the students whose name begins with a specific letter.
+
+#Modify your program to only print the students whose name is shorter than 12 characters.
