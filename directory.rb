679ed307bef3f1fada2ca95c726ed70f198b56ca
Oleg Grushetskyy <olegfkl@gmail.com>
2017-10-09 11:48:27 +0100

    Add list of students to directory

---
+++ directory.rb
@@ -0,0 +1,17 @@
+student_count = 11
+# We print the list of students
+puts "The students of Villains Academy"
+puts "--------------"
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex DeLarge"
+puts "The Wicked Witch of the West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Joffrey Baratheon"
+puts "Norman Bates"
+ # We print the total number of students
+puts "Overall, we have #{student_count} great students"
